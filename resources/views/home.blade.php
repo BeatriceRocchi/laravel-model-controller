e143ca53e807fbc0110746a3e9d6d105494d0f31
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-10 17:20:20 +0200

    [add] header style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,15 @@
         <div class="row row-cols-4">
             @foreach ($movies as $movie)
                 <div class="col my-3">
-                    <div class="card">
-                        <div class="card-header text-center fw-semibold">
-                            <a href="{{ route('movie-details', ['id' => $movie->id]) }}">{{ $movie->title }}</a>
+                    <div class="card custom_card">
+                        <a href="{{ route('movie-details', ['id' => $movie->id]) }}"><img src="{{ $movie->img }}"
+                                class="card-img-top" alt="{{ $movie->title }}"></a>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
                         </div>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">Original title: {{ $movie->original_title }}</li>
                             <li class="list-group-item">Language: {{ $movie->nationality }}</li>
-                            <li class="list-group-item">Date: {{ $movie->date }}</li>
                             <li class="list-group-item">Vote: {{ $movie->vote }}</li>
                         </ul>
                     </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,13 @@
 <header>
-    <h2>Header</h2>
-    <a href="{{ route('home') }}">Today's movies</a>
-    <a href="{{ route('best-movies') }}">Critically acclaimed movies</a>
-    <a href="{{ route('recent-movies') }}">Movies from the 90's</a>
+    <nav class="navbar navbar-custom navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="{{ route('home') }}">Today's movies</a>
+                    <a class="nav-link" href="{{ route('best-movies') }}">Critically acclaimed movies</a>
+                    <a class="nav-link" href="{{ route('recent-movies') }}">Movies from the 90's</a>
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
