cc4884a1e72e034ee0bbb29461d56325fc293ae6
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-10 15:44:40 +0200

    [add] cards in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,24 @@
 
 @section('content')
     <h1>Movies List</h1>
+    <div class="container">
+        <div class="row row-cols-4">
+            @foreach ($movies as $movie)
+                <div class="col my-3">
+                    <div class="card">
+                        <div class="card-header text-center fw-semibold">
+                            {{ $movie->title }}
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Original title: {{ $movie->original_title }}</li>
+                            <li class="list-group-item">Language: {{ $movie->nationality }}</li>
+                            <li class="list-group-item">Date: {{ $movie->date }}</li>
+                            <li class="list-group-item">Vote: {{ $movie->vote }}</li>
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+    </div>
 @endsection
